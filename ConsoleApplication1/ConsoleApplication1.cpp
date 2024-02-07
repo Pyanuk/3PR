893fd06f58737a231e4f7393c4ef15f7601a0d39
Pyanuk <rhalikov735@gmail.com>
2024-02-07 10:41:09 +0300

    3PR

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+
+
+double ringArea(double innerRadius, double outerRadius) {
+    const double pi = 3.14;
+    double area = pi * (outerRadius * outerRadius - innerRadius * innerRadius);
+    return area;
+}
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    double inner, outer;
+    cout << "Введите внутренний радиус: ";
+    cin >> inner;
+    cout << "Введите внешний радиус: ";
+    cin >> outer;
+    cout << "Площадь кольца равна: " << ringArea(inner, outer);
+    return 0;
+}
